bdd44240e6a9ea0cba03b0e02ddc147697f8f35d
Cindy Vo <yendai.vo@hotmail.com>
2018-07-14 22:40:06 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   winner_name = ""
   data[season].each { |contestant|
-    if contestant.status == "Winner"
+    if contestant["status"] == "Winner"
       winner_name = contestant.name.split(" ")[0]
     end
   }
