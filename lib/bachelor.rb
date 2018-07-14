76994f72eb3a4168b721a5ca27c7ec670a73c913
Cindy Vo <yendai.vo@hotmail.com>
2018-07-14 22:56:54 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,55 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner_name = ""
+  data[season].each { |contestant|
+    if contestant["status"] == "Winner"
+      winner_name = contestant["name"].split(" ")[0]
+    end
+  }
+  winner_name
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  woman_name = ""
+  data.each { |season, woman_array|
+    woman_array.each { |woman|
+      if woman["occupation"] == occupation
+        woman_name = woman["name"]
+      end
+    }
+  }
+  woman_name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  woman_count = 0
+  data.each { |season, woman_array|
+    woman_array.each { |woman|
+      if woman["hometown"] == hometown
+        woman_count+=1
+      end
+    }
+  }
+  woman_count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupation = ""
+  data.each { |season, woman_array|
+    woman_array.each { |woman|
+      if woman["hometown"] == hometown  && occupation == ""
+        occupation = woman["occupation"]
+      end
+    }
+  }
+  occupation
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total_age = 0.00
+  contestants = 0.00
+  data[season].each {|contestant|
+    total_age += contestant["age"].to_f
+    contestants += 1
+  }
+  (total_age/contestants).round
 end
